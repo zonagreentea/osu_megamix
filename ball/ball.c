1ccbe3104bc624548ff606fb55ae7e3018ff40fa
Shizuka Jace Russel Simon <zonagreentea@ZonaBook-Pro.local>
2025-12-29 21:15:02 -0500

    ball 1 iOS

--- ball/ball.c
+++ ball/ball.c
@@ -1,5 +1,19 @@
 #include "ball.h"
-static inline uint32_t ball_xorshift32(uint32_t x){ x^=x<<13; x^=x>>17; x^=x<<5; return x; }
-void ball_init(ball_world_t* w, uint32_t seed){ w->s.tick=0; w->s.rng=seed?seed:1; }
-void ball_step(ball_world_t* w){ w->s.tick+=1; w->s.rng=ball_xorshift32(w->s.rng); }
-const ball_state_t* ball_state(const ball_world_t* w){ return &w->s; }
+
+static inline uint32_t ball_xorshift32(uint32_t x){
+  x ^= x << 13; x ^= x >> 17; x ^= x << 5; return x;
+}
+
+void ball_init(ball_world_t* w, uint32_t seed){
+  w->s.tick = 0;
+  w->s.rng  = seed ? seed : 1;
+}
+
+void ball_step(ball_world_t* w){
+  w->s.tick += 1;
+  w->s.rng = ball_xorshift32(w->s.rng);
+}
+
+const ball_state_t* ball_state(const ball_world_t* w){
+  return &w->s;
+}
--- ball/ball.h
+++ ball/ball.h
@@ -1,9 +1,23 @@
 #pragma once
 #include <stdint.h>
+
 typedef uint32_t ball_tick_t;
-typedef struct { ball_tick_t tick; uint32_t buttons; int16_t ax0, ay0; } ball_input_t;
-typedef struct { ball_tick_t tick; uint32_t rng; } ball_state_t;
-typedef struct { ball_state_t s; } ball_world_t;
+
+typedef struct {
+  ball_tick_t tick;
+  uint32_t buttons;
+  int16_t ax0, ay0;
+} ball_input_t;
+
+typedef struct {
+  ball_tick_t tick;
+  uint32_t rng;
+} ball_state_t;
+
+typedef struct {
+  ball_state_t s;
+} ball_world_t;
+
 void ball_init(ball_world_t* w, uint32_t seed);
 void ball_step(ball_world_t* w);
 const ball_state_t* ball_state(const ball_world_t* w);
