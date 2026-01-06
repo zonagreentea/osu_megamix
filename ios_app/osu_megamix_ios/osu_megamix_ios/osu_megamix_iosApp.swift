6a8722ef99d7c8b72fa1078fff3506c200997563
Shizuka Jace Russel Simon <zonagreentea@ZonaBook-Pro.local>
2026-01-06 00:20:52 -0500

    Add iOS app project for 1b ball (Xcode project)

---
+++ ios_app/osu_megamix_ios/osu_megamix_ios/osu_megamix_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  osu_megamix_iosApp.swift
+//  osu_megamix_ios
+//
+//  Created by Shizuka Jace Russel Simon on 2026-01-06.
+//
+
+import SwiftUI
+
+@main
+struct osu_megamix_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
